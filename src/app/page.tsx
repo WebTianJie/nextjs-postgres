4f1161cdb5495b250ec796b2f9b6d5e143acc187
gaorui-v <Jie.Chen1@budweiserapac.com>
2026-05-11 17:39:43 +0800

    feat:Postgres 数据库,代码里的增删改查

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,56 @@
-import Image from "next/image";
+import { neon } from "@neondatabase/serverless";
+import { revalidatePath } from "next/cache";
 
-export default function Home() {
+const databaseUrl = process.env.DATABASE_URL;
+const sql = databaseUrl ? neon(databaseUrl) : null;
+
+export default async function Home() {
+    const result = await sql?.query("SELECT * FROM users");
+    console.log("results", result);
+
+    async function createFunction(formData: FormData) {
+        "use server";
+        const name = formData.get("name");
+        const age = formData.get("age");
+        await sql?.query("INSERT INTO users (name, age) VALUES ($1, $2)", [name, age]);
+        revalidatePath("/");
+    }
+
+    async function deleteFunction(formData: FormData) {
+        "use server";
+        const id = formData.get("id");
+        await sql?.query("DELETE FROM users WHERE id = $1", [id]);
+        revalidatePath("/");
+    }
+
     return (
-        <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-            <h1>蓝火之瞳</h1>
+        <div>
+            <h1>Postgres 数据库</h1>
+            <div>
+                <form action={createFunction}>
+                    <label htmlFor="name">Name:</label>
+                    <input type="text" className="border-1" id="name" name="name" required />
+                    <label htmlFor="age">Age:</label>
+                    <input type="number" className="border-1" id="age" name="age" required />
+                    <button type="submit" className="cursor-pointer">
+                        Create User
+                    </button>
+                </form>
+                {result?.map((item: any, index: number) => (
+                    <div key={item.id} className="flex items-center gap-4 p-2 border-b">
+                        <div className="flex-1">
+                            <p>{item.name}</p>
+                            <p>{item.age}</p>
+                        </div>
+                        <form action={deleteFunction}>
+                            <input type="hidden" name="id" value={item.id} />
+                            <button type="submit" className="px-3 py-1 bg-red-500 text-white rounded hover:bg-red-600 cursor-pointer">
+                                删除
+                            </button>
+                        </form>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
